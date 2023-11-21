6701d4380b00e2aa5066413509d45a6fd011bc8f
Kishlay Krishna <69421373+KrisJarvis@users.noreply.github.com>
2023-11-21 18:17:51 +0530

    Time: 85 ms (5.34%), Space: 55.5 MB (18.45%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+        return solve(n);
+    }
+    
+    vector<int> solve(int n){
+        //base case
+        if(n==1) {
+            vector<int> k = {1};
+            return k;
+        }
+        
+        vector<int> ans = solve(n-1);
+        vector<int> temp;
+        for(int i = 0; i < ans.size(); i++){
+            if(2*ans[i]-1 <= n) temp.push_back(2*ans[i]-1);
+        }
+        for(int i = 0; i < ans.size(); i++){
+            if(2*ans[i] <= n) temp.push_back(2*ans[i]);
+        }
+        return temp;
+    }
+};
