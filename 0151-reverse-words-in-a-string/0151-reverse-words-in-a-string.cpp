4830ad7d5dc00a287622766d342db547cf77bd27
Kishlay Krishna <69421373+KrisJarvis@users.noreply.github.com>
2024-01-26 14:17:27 +0530

    Time: 11 ms (19.44%), Space: 23.3 MB (12.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,17 +1,19 @@
+#include <sstream>
+
 class Solution {
 public:
     string reverseWords(string s) {
-        reverse(s.begin(), s.end());
-        istringstream iss (s);
-        string res;
-        string tmp;
-        while(iss>>tmp){
-            reverse(tmp.begin(), tmp.end());
-            res+=(' '+tmp);
+        stringstream ss(s);
+        string temp, result = "";
+        bool first = true;
+        while(ss >> temp)
+        {
+            if(first)
+                first = false;
+            else
+                temp = temp + " ";
+            result = temp + result;
         }
-        if (res.size()>0){
-            res = res.substr(1);
-        }
-        return res;
+        return result;
     }
 };
